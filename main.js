021a8ae9d8ab811f447ac8880f7cfa78f6a320d7
javicent <103406577+javicent@users.noreply.github.com>
2022-10-13 00:24:20 -0700

    Platform changes
    I need the color mechanic to work to traverse between platforms

--- main.js
+++ main.js
@@ -58,13 +58,16 @@ let blueCube;
 /** @type {{pos: Vector, size: Vector}}*/
 let platform;
 
+let currentLevel = 1;
+
 function update() {
   //startup function
   if (!ticks) {
     player = { pos: vec(10, G.HEIGHT - 10), vx: 0, ty: 90 };
     redCube = { pos: vec(4*G.WIDTH/5, 14*G.HEIGHT/16), speed: 1};
     blueCube = { pos: vec(4*G.WIDTH/5, 12*G.HEIGHT/16), speed: 1};
-    platform = { pos: vec(4*G.WIDTH/5, 16*G.HEIGHT/16), size: vec(G.WIDTH, 10)};
+    redPlatform = { pos: vec(4*G.WIDTH/5, 16*G.HEIGHT/16), size: vec(G.WIDTH, 10)};
+    bluePlatform = { pos: vec(4*G.WIDTH/5, 16*G.HEIGHT/16), size: vec(G.WIDTH, 10)};
   }
   
   color("black")
@@ -76,12 +79,28 @@ function update() {
   color("light_black");
   rect(0, G.HEIGHT - 47, G.WIDTH, 5);
 
-  if (input.isPressed && player.pos.y > 5) {
-    player.pos.y -= 1;
-  }else{
-    if(!player.pos.isInRect(0, G.HEIGHT - 50, G.WIDTH, 5)){
-      player.pos.y += 1;
-    }
+  //red platform
+
+  if(currentLevel == 1){
+    if (input.isPressed && player.pos.y > G.HEIGHT - 40) {
+      player.pos.y -= 1;
+      if(player.pos.y < G.HEIGHT - 50){
+        currentLevel --;
+      }
+    }else{
+      if(!player.pos.isInRect(0, G.HEIGHT - 13, G.WIDTH, 100)){
+        player.pos.y += 1;
+      }
+    }    
+  }
+  if(currentLevel == 2){
+    if (input.isPressed && player.pos.y > 5) {
+      player.pos.y -= 1;
+    }else{
+      if(!player.pos.isInRect(0, G.HEIGHT - 50, G.WIDTH, 5)){
+        player.pos.y += 1;
+      }
+    }    
   }
 
 
